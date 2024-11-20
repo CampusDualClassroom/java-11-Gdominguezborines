16a5dd8dc4f0b17160521c55401aa11eb403197c
Gabriel <Gabrieldb050890@gmail.com>
2024-11-20 20:51:02 +0100

    Ejercicio hecho

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -3,9 +3,41 @@ package com.campusdual.classroom;
 public class Exercise11 {
     public static void main(String[] args) {
         TVRemote redRemote = new TVRemote("Rojo");
-        TVRemote blackRemote = new TVRemote("Negro");
-        System.out.println(redRemote.getColor());
-        System.out.println(blackRemote.getColor());
+
+
+        /* Encender la TV
+        Subir un canal
+        Bajar un canal*/
+
+        redRemote.turnOn();
+        redRemote.channelUp();
+        redRemote.channelDown();
+
+        // Establecer el canal en 0, intentar bajar un canal y comprobar que no se puede
+        redRemote.channel=0;
+        redRemote.channelDown();
+
+        /*Subir el volumen
+        Bajar el volumen*/
+
+        redRemote.volumeUp();
+        redRemote.volumeDown();
+
+        //  Establecer el volumen en 0, intentar bajar el volumen y comprobar que no se puede
+        redRemote.volume=0;
+        redRemote.volumeDown();
+
+        // Obtener el color del mando a distancia y mostrarlo por pantalla
+
+        System.out.println("Color: "+redRemote.getColor());
+
+        //     Apagar la TV
+
+        redRemote.turnOff();
+
+
+
+
     }
 
 }
